623f426259cf8692265cb61f2488a3330cdecc4f
Vasu <vasukodati05@gmail.com>
2024-10-25 17:40:46 +0530

    added deletion to avl

--- logic/avltreeindex.js
+++ logic/avltreeindex.js
@@ -51,3 +51,18 @@ function initializeZoom(){
     search(searchKey);
     
 }
+function deleteNode()
+{
+    var deleteInpt = document.getElementById("inptkey").value;
+    if(deleteInpt == "")
+    {
+        return;
+    }
+    deleteKey(parseInt(deleteInpt));
+    console.log(JSON.stringify(Tree.toJSON(),null,2));
+    
+    //Tree.rebalanceTree();
+   // Tree.removeTree();
+   
+    //drawTree(Tree.toJSON(),Tree.size);
+}
